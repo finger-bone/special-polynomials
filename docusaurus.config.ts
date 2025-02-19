dbc5ef92e87f91700a93045baa8cd50249618bc2
Zend <ZendWang040302@outlook.com>
2025-02-19 13:46:51 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,7 +64,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Notes',
         },
       ],
     },
